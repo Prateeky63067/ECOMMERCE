0123d89ed1e142df3fa86f8cb3440e04b0da1e5a
Prateek yadav <shubhamy9451@gmail.com>
2023-07-07 18:52:33 +0530

    6.59 pagination done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,8 @@ import Footer from "./component/layout/Footer/Footer.js";
 import Home from "./component/Home/Home.js";
 import ProductDetails from "./component/Product/ProductDetails.js";
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+// import Search from "./component/Product/Search.js"
 import Products from './component/Product/Products.js'
-import Search from "./component/Product/Search.js"
 function App() {
   return (
     <>
@@ -16,7 +16,7 @@ function App() {
       <Route path='/product/:id' element={<ProductDetails/>}/>
       <Route path='/products' element={<Products/>}/>
       <Route path='/products/:keyword' element={<Products/>}/>
-      <Route path='/search' element={<Search/>}/>
+      {/* <Route path='/search' element={<Search/>}/> */}
       
       </Routes>
       <Footer/>
--- frontend/src/component/Product/Products.js
+++ frontend/src/component/Product/Products.js
@@ -1,19 +1,27 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import "./Products.css";
 import { useSelector, useDispatch } from "react-redux";
 import { clearErrors, getProduct } from "../../actions/productAction";
 import ProductCard from "../Home/ProductCard";
 import Loader from "../layout/Loader/Loader";
 import { useParams } from "react-router-dom";
+import Pagination from "react-js-pagination";
+
 
 const Products = () => {
    
+  
   const dispatch=useDispatch()
+  const [currentPage,setCurrentPage]=useState(1);
   const { keyword } = useParams();
-    const {products,loading,error,productsCount}=useSelector(state=>state.products)
+    const {products,loading,error,productsCount,resultPerPage}=useSelector(state=>state.products)
+
+    const setCurrentPageNo=(e)=>{
+setCurrentPage(e);
+    }
   useEffect(() => {
-    dispatch(getProduct(keyword));
-  }, [dispatch,keyword]);
+    dispatch(getProduct(keyword,currentPage));
+  }, [dispatch,keyword,currentPage]);
 
   return <Fragment>{loading ? <Loader /> :
    <Fragment>
@@ -26,6 +34,26 @@ const Products = () => {
     ))
    }
    </div>
+  {
+    resultPerPage<productsCount &&<div className="paginationBox">
+     <Pagination
+      activePage={currentPage}
+      itemsCountPerPage={resultPerPage}
+      totalItemsCount={productsCount}
+      onChange={setCurrentPageNo}
+      nextPageText="Next"
+      prevPageText="Prev"
+      firstPageText="1st"
+      lastPageText="Last"
+      itemClass="page-item"
+      linkClass="page-link"
+      activeClass="pageItemActive"
+      activeLinkClass="pageLinkActive"
+     />
+
+  </div>
+  }
+
   </Fragment>}
   
   </Fragment>;
