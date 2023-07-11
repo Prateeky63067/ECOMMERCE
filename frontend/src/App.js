0e31f3fee0bc282f6a92b3e8f6cc1e4efee1a49d
Prateek yadav <shubhamy9451@gmail.com>
2023-07-11 15:54:49 +0530

    8.33

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import React from "react";
 import Header from "./component/layout/Header/Header.js";
 import Footer from "./component/layout/Footer/Footer.js";
 import Home from "./component/Home/Home.js";
@@ -7,12 +8,25 @@ import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
 // import Search from "./component/Product/Search.js"
 import Products from './component/Product/Products.js'
 import LoginSignUp from "./component/User/LoginSignUp";
+import store from "./store"
+import { loadUser } from "./actions/userAction";
+import UserOptions from "./component/layout/Header/UserOptions.js"
+import { useSelector } from "react-redux";
+
+
 
 function App() {
+
+  const {isAuthenticated,user}=useSelector((state)=>state.user)
+
+  React.useEffect(() => {
+  store.dispatch(loadUser());
+  }, [])
   return (
     <>
     <Router>
      <Header />
+      {isAuthenticated && <UserOptions user={user}/>}
       <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/product/:id' element={<ProductDetails/>}/>
