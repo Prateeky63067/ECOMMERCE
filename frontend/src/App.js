b402b7954c981ba6acdcb65e77ba80cfbe7c747d
Prateek yadav <shubhamy9451@gmail.com>
2023-07-13 09:50:46 +0530

    9.48 Change, Forgot & Reset Password

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,10 @@ import { useSelector } from "react-redux";
 import Profile from "./component/User/Profile.js";
 import ProtectedRoute from "./component/Route/ProtectedRoute";
 import UpdateProfile from "./component/User/UpdateProfile.js"
+import UpdatePassword from "./component/User/UpdatePassword.js"
+import ForgotPassword from "./component/User/ForgotPassword.js"
+import ResetPassword from "./component/User/ResetPassword.js"
+
 function App() {
   const { isAuthenticated, user } = useSelector((state) => state.user);
 
@@ -33,6 +37,9 @@ function App() {
           <Route path="/products/:keyword" component={Products} />
           <ProtectedRoute exact path="/account" component={Profile} />
           <ProtectedRoute exact path="/me/update" component={UpdateProfile} />
+          <ProtectedRoute exact path="/password/update" component={UpdatePassword} />
+          <Route exact path="/password/forgot" component={ForgotPassword} />
+          <Route exact path="/password/reset/:token" component={ResetPassword} />
           <Route exact path="/login" component={LoginSignUp} />
         </Switch>
         <Footer />
--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -1,61 +1,69 @@
 import {
-    LOGIN_REQUEST,
-    LOGIN_FAIL,
-    LOGIN_SUCCESS,
-    REGISTER_USER_REQUEST,
-    REGISTER_USER_SUCCESS,
-    REGISTER_USER_FAIL,
-    LOAD_USER_REQUEST,
-    LOAD_USER_SUCCESS,
-    LOAD_USER_FAIL,
-    LOGOUT_SUCCESS,
-    LOGOUT_FAIL,
-    UPDATE_PROFILE_REQUEST,
-    UPDATE_PROFILE_SUCCESS,
-    UPDATE_PROFILE_FAIL,
-    UPDATE_PROFILE_RESET,
-    CLEAR_ERRORS,
-  } from "../constants/userConstants";
-  import axios from "axios";
+  LOGIN_REQUEST,
+  LOGIN_FAIL,
+  LOGIN_SUCCESS,
+  REGISTER_USER_REQUEST,
+  REGISTER_USER_SUCCESS,
+  REGISTER_USER_FAIL,
+  LOAD_USER_REQUEST,
+  LOAD_USER_SUCCESS,
+  LOAD_USER_FAIL,
+  LOGOUT_SUCCESS,
+  LOGOUT_FAIL,
+  UPDATE_PROFILE_REQUEST,
+  UPDATE_PROFILE_SUCCESS,
+  UPDATE_PROFILE_FAIL,
+  UPDATE_PROFILE_RESET,
+  UPDATE_PASSWORD_REQUEST,
+  UPDATE_PASSWORD_SUCCESS,
+  UPDATE_PASSWORD_FAIL,
+  FORGOT_PASSWORD_REQUEST,
+  FORGOT_PASSWORD_SUCCESS,
+  FORGOT_PASSWORD_FAIL,
+  RESET_PASSWORD_REQUEST,
+  RESET_PASSWORD_SUCCESS,
+  RESET_PASSWORD_FAIL,
+  CLEAR_ERRORS,
+} from "../constants/userConstants";
+import axios from "axios";
 
-  // Login
+// Login
 export const login = (email, password) => async (dispatch) => {
-    try {
-      dispatch({ type: LOGIN_REQUEST });
-  
-      const config = { headers: { "Content-Type": "application/json" } };
-  
-      const { data } = await axios.post(
-        `/api/v1/login`,
-        { email, password },
-        config
-      );
-  
-      dispatch({ type: LOGIN_SUCCESS, payload: data.user });
-    } catch (error) {
-      dispatch({ type: LOGIN_FAIL, payload: error.response.data.message });
-    }
-  }; 
+  try {
+    dispatch({ type: LOGIN_REQUEST });
 
-  export const register = (userData) => async (dispatch) => {
-    try {
-      dispatch({ type: REGISTER_USER_REQUEST });
-  
-      const config = { headers: { "Content-Type": "multipart/form-data" } };
-  
-      const { data } = await axios.post(`/api/v1/register`, userData, config);
-  
-      dispatch({ type: REGISTER_USER_SUCCESS, payload: data.user });
-    } catch (error) {
-      dispatch({
-        type: REGISTER_USER_FAIL,
-        payload: error.response.data.message,
-      });
-    }
-  };
+    const config = { headers: { "Content-Type": "application/json" } };
 
+    const { data } = await axios.post(
+      `/api/v1/login`,
+      { email, password },
+      config
+    );
 
-  // Load User
+    dispatch({ type: LOGIN_SUCCESS, payload: data.user });
+  } catch (error) {
+    dispatch({ type: LOGIN_FAIL, payload: error.response.data.message });
+  }
+};
+
+export const register = (userData) => async (dispatch) => {
+  try {
+    dispatch({ type: REGISTER_USER_REQUEST });
+
+    const config = { headers: { "Content-Type": "multipart/form-data" } };
+
+    const { data } = await axios.post(`/api/v1/register`, userData, config);
+
+    dispatch({ type: REGISTER_USER_SUCCESS, payload: data.user });
+  } catch (error) {
+    dispatch({
+      type: REGISTER_USER_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
+
+// Load User
 export const loadUser = () => async (dispatch) => {
   try {
     dispatch({ type: LOAD_USER_REQUEST });
@@ -96,9 +104,69 @@ export const updateProfile = (userData) => async (dispatch) => {
     });
   }
 };
+// Update Password
+export const updatePassword = (passwords) => async (dispatch) => {
+  try {
+    dispatch({ type: UPDATE_PASSWORD_REQUEST });
 
+    const config = { headers: { "Content-Type": "application/json" } };
 
-  // Clearing Errors
+    const { data } = await axios.put(
+      `/api/v1/password/update`,
+      passwords,
+      config
+    );
+
+    dispatch({ type: UPDATE_PASSWORD_SUCCESS, payload: data.success });
+  } catch (error) {
+    dispatch({
+      type: UPDATE_PASSWORD_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
+
+// Forgot Password
+export const forgotPassword = (email) => async (dispatch) => {
+  try {
+    dispatch({ type: FORGOT_PASSWORD_REQUEST });
+
+    const config = { headers: { "Content-Type": "application/json" } };
+
+    const { data } = await axios.post(`/api/v1/password/forgot`, email, config);
+
+    dispatch({ type: FORGOT_PASSWORD_SUCCESS, payload: data.message });
+  } catch (error) {
+    dispatch({
+      type: FORGOT_PASSWORD_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
+
+// Reset Password
+export const resetPassword = (token, passwords) => async (dispatch) => {
+  try {
+    dispatch({ type: RESET_PASSWORD_REQUEST });
+
+    const config = { headers: { "Content-Type": "application/json" } };
+
+    const { data } = await axios.put(
+      `/api/v1/password/reset/${token}`,
+      passwords,
+      config
+    );
+
+    dispatch({ type: RESET_PASSWORD_SUCCESS, payload: data.success });
+  } catch (error) {
+    dispatch({
+      type: RESET_PASSWORD_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
+
+// Clearing Errors
 export const clearErrors = () => async (dispatch) => {
-    dispatch({ type: CLEAR_ERRORS });
-  }; 
+  dispatch({ type: CLEAR_ERRORS });
+};
